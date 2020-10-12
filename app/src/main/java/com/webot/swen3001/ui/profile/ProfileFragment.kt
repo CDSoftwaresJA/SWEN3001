be190187438bb723470e90bebe382fd6b674cdcb
Jason Johnson <jvsonjohnson@gmail.com>
2020-10-11 21:48:59 -0500

    *Working* Added an event listener to the Update My Status card and Exposure Notifications Card with intents for new activities

--- app/src/main/java/com/webot/swen3001/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/webot/swen3001/ui/profile/ProfileFragment.kt
@@ -1,11 +1,15 @@
 package com.webot.swen3001.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.webot.swen3001.ExposureNotifications
 import com.webot.swen3001.R
+import com.webot.swen3001.UpdateStatus
+import kotlinx.android.synthetic.main.fragment_notifications.*
 
 class ProfileFragment : Fragment() {
 
@@ -17,6 +21,22 @@ class ProfileFragment : Fragment() {
   ): View? {
 
 
-    return inflater.inflate(R.layout.fragment_notifications, container, false)
+    val rootView: View = inflater.inflate(R.layout.fragment_notifications, container, false)
+
+    //Adding event listener to the Update Status Card on Profile Screen
+    val updateStatusCard: View = rootView.findViewById(R.id.mystatus_card)
+    updateStatusCard.setOnClickListener {
+      val updateMyStatusIntent = Intent(requireContext(), UpdateStatus::class.java)
+      startActivity(updateMyStatusIntent)
+    }
+
+    //Adding event listener to the Exposure Notifications Card on Profile Screen
+    val expNotificationsCard: View = rootView.findViewById(R.id.expNotifications_card)
+    expNotificationsCard.setOnClickListener {
+      val expNotificationsIntent = Intent(requireContext(), ExposureNotifications::class.java)
+      startActivity(expNotificationsIntent)
+    }
+
+    return rootView
   }
 }
