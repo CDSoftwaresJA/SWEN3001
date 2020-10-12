6deb74805cc6575b00fc8300c466a0d3ad6dd413
Jason Johnson <jvsonjohnson@gmail.com>
2020-10-11 21:09:06 -0500

    Added an event listener to the Update My Status card and Exposure Notifications Card with intents for new activities

---
+++ app/src/main/java/com/webot/swen3001/ExposureNotifications.kt
@@ -0,0 +1,11 @@
+package com.webot.swen3001
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExposureNotifications : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exposure_notifications)
+    }
+}
--- app/src/main/java/com/webot/swen3001/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/webot/swen3001/ui/profile/ProfileFragment.kt
@@ -1,11 +1,14 @@
 package com.webot.swen3001.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.webot.swen3001.ExposureNotifications
 import com.webot.swen3001.R
+import com.webot.swen3001.UpdateStatus
 
 class ProfileFragment : Fragment() {
 
@@ -16,6 +19,22 @@ class ProfileFragment : Fragment() {
     savedInstanceState: Bundle?
   ): View? {
 
-    return inflater.inflate(R.layout.fragment_notifications, container, false)
+    val rootView: View = inflater.inflate(R.layout.fragment_notifications, container, false)
+
+    //Adding event listener to the Update My Status card
+    val myStatusCard: View = rootView.findViewById(R.id.mystatus_card)
+    myStatusCard.setOnClickListener {
+      val intent = Intent(requireContext(),UpdateStatus::class.java)
+      startActivity(intent)
+    }
+
+
+    //Adding event listener to the Exposure Notifications card
+    val expNotificationsCard: View = rootView.findViewById(R.id.expNotifications_card)
+    expNotificationsCard.setOnClickListener {
+      val intent = Intent(requireContext(),ExposureNotifications::class.java)
+      startActivity(intent)
+    }
+    return rootView
   }
 }
