4ac9551c9ef70132c14ae6b9bfc0f97341268cb7
paulrsmithjnr <paulrsmithjnr@gmail.com>
2020-11-17 13:31:59 -0500

    Added recycler view to health report screen

--- app/src/main/java/com/webot/swen3001/ui/report/ReportFragment.kt
+++ app/src/main/java/com/webot/swen3001/ui/report/ReportFragment.kt
@@ -10,9 +10,14 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.webot.swen3001.MainActivity
 import com.webot.swen3001.R
 import com.webot.swen3001.ui.SymptomsActivity
+import com.webot.swen3001.ui.SymptomsChecklistAdapter
+import com.webot.swen3001.ui.SymptomsListAdapter
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 
 class ReportFragment : Fragment() {
 
@@ -23,6 +28,11 @@ class ReportFragment : Fragment() {
     savedInstanceState: Bundle?
   ): View? {
     val rootView = inflater.inflate(R.layout.fragment_dashboard, container, false)
+
+    rootView.findViewById<RecyclerView>(R.id.symptoms_list_recycler_view).adapter = SymptomsListAdapter()
+    rootView.findViewById<RecyclerView>(R.id.symptoms_list_recycler_view).layoutManager = LinearLayoutManager(context)
+    rootView.findViewById<RecyclerView>(R.id.symptoms_list_recycler_view).setHasFixedSize(true)
+
     val submitButton = rootView.findViewById<Button>(R.id.add_reportbtn)
     submitButton.setOnClickListener {
       val intent = Intent(requireContext(), SymptomsActivity::class.java)
